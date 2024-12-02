d13ac96c6d78758286cdbc127bc63526786c5ee5
Subash Parajuli <subashparajuli007@gmail.com>
2024-12-02 19:56:53 +0000

    'RemoteFeedLoader' doesn't request data upon creation

---
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Subash Parajuli on 02/12/2024.
+//
+
+import Testing
+
+struct RemoteFeedLoaderTests {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
