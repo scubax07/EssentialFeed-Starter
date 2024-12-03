322654d25a0b92486d2c1b96167e6f2b8df8e223
Subash Parajuli <subashparajuli007@gmail.com>
2024-12-03 13:45:02 +0000

    improve test names

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -19,7 +19,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         sut.load()
