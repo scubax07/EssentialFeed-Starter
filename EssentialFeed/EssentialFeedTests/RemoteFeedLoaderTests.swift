b58a0b1091120dc64aeceb290193bb8946ac5131
Subash Parajuli <subashparajuli007@gmail.com>
2025-01-07 14:40:43 +0000

    replace optional params with enums with success and failure cases.

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -87,19 +87,19 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         
-        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
         
 
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse? ) -> Void ) {
-            messages.append( (url, completion) )
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void ) {
+            messages.append((url, completion ))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode statusCode: Int, at index: Int = 0) {
@@ -108,8 +108,9 @@ class RemoteFeedLoaderTests: XCTestCase {
                 statusCode: statusCode,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            
+            messages[index].completion( .success(response))
         }
     }
    
