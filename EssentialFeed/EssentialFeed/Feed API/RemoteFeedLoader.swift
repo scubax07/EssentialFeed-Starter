97071ef0d3e81dce5fbf0d1d063e090d190b2e61
Subash Parajuli <subashparajuli007@gmail.com>
2025-01-11 23:28:47 +0000

    move Decodable logic to a new private Item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -56,7 +56,25 @@ public final class RemoteFeedLoader {
     }
     
     private struct Root: Decodable {
-        let items: [FeedItem]
+        let items: [Item]
+    }
+    
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+                
+        var item: FeedItem {
+            return FeedItem(
+                id: id,
+                description: description,
+                location: location,
+                imageURL: image
+            )
+            
+        }
+        
     }
     
 }
